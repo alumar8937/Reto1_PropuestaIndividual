6a1e59c284e6977e5d96c2824228e510257c5400
pedro09pm <pedro09pm.principal@gmail.com>
2022-12-12 23:10:32 +0100

    Crear Usuarios Fix

--- SistemaPropuesto/crearUsuarios.ps1
+++ SistemaPropuesto/crearUsuarios.ps1
@@ -1,7 +1,7 @@
 $file_users=Import-Csv -Path usuarios.csv 
 foreach ($user in $file_users) { 
   $clave=ConvertTo-SecureString $user.contrasenya -AsPlainText -Force
-  New-LocalUser $user.cuenta -Password $clave -Description $user.descripcion -FullName $user.nombre -AccountNeverExpires -PasswordNeverExpires $false
+  New-LocalUser $user.cuenta -Password $clave -Description $user.descripcion -FullName $user.nombre -AccountNeverExpires
   #Añadimos la cuenta de usuario en el grupo de Usuarios del sistema
   Add-LocalGroupMember -Group Users -Member $user.cuenta
   Add-LocalGroupMember -Group $user.departamento -Member $user.cuenta
